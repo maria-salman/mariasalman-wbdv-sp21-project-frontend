c25ef9079b8428ec5f82b94d07513c2fc24662bb
MeeraSurendran <meera02712@gmail.com>
2021-04-17 14:05:01 -0400

    "Adding search"

--- src/components/search.js
+++ src/components/search.js
@@ -1,9 +1,19 @@
 import React from "react";
 
-
 const Search = () => {
     return (
-        <h1>Search</h1>
+        <div className= "container">
+            <h1 className = "search grid">Search</h1>
+            <div>Let's get started</div>
+            <input
+                onChange={this.handleChange}
+                name = "searchbox"
+                className="form-control font-italic"
+                placeholder="Enter the place you  want to serach"
+            />
+
+        </div>
+
     )
 }
 
